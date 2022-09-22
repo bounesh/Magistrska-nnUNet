ac7f66b939319d8245b4e270c0dbcf07c50cc4ba
Bounesh <eb1690@student.uni-lj.si>
2022-09-22 17:30:14 +0300

    Fixed dataset.json

--- data/run_preprocess.sh
+++ data/run_preprocess.sh
@@ -1,9 +1,6 @@
-export nnUNet_raw_data_base="/ceph2/grid/home/mpesek/cell/nnunet/nnUNet_raw_data_base"
-
-export nnUNet_preprocessed="/ceph2/grid/home/mpesek/cell/nnunet/nnUNet_preprocessed"
-
-export RESULTS_FOLDER="/ceph2/grid/home/mpesek/cell/nnunet/nnUNet_trained_models"
-
+export nnUNet_raw_data_base="D:\Dokumenti\Magistrska\Magistrska-nnUNet\data\nnUNet_raw_data_base"
+export nnUNet_preprocessed="D:\Dokumenti\Magistrska\Magistrska-nnUNet\data\nnUNet_preprocessed"
+export RESULTS_FOLDER="D:\Dokumenti\Magistrska\Magistrska-nnUNet\data\nnUNet_trained_models"
 export PATH="/ceph2/grid/home/mpesek/.local/bin:$PATH"
 
 CUDA_VISIBLE_DEVICES=$1
