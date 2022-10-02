3b8c94e92df8627acdc903729e217586c53ef260
bounesh <eb1690@student.uni-lj.si>
2022-10-02 20:36:10 +0300

    Progress update 2.10. 20:36

--- data/runNnunet3df_uterus.sh
+++ data/runNnunet3df_uterus.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=$1
+CUDA_VISIBLE_DEVICES=0
 
-nnUNet_train 3d_fullres nnUNetTrainerV2 Task503_Uterus $2 -c --npz
+nnUNet_train 3d_fullres nnUNetTrainerV2 Task503_Uterus $1 -c --npz
