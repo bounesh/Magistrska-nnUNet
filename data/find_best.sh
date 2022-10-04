a39920bbd399a33bdd999e44d0ad371c6fb5ceca
bounesh <eb1690@student.uni-lj.si>
2022-10-04 09:14:56 +0300

    Progress update 4.10. 09:14

--- data/find_best.sh
+++ data/find_best.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=$1
+CUDA_VISIBLE_DEVICES=0
 
 nnUNet_find_best_configuration -m 2d 3d_fullres -t 503
--- data/predict.sh
+++ data/predict.sh
@@ -1,9 +1,1 @@
-export OUTPUT_FOLDER_MODEL1="/ceph2/grid/home/mpesek/cell/nnunet/nnUNet_raw_data_base/Task501-results_3d_fullres"
-
-export FOLDER_WITH_TEST_CASES="/ceph2/grid/home/mpesek/cell/nnunet/nnUNet_raw_data_base/nnUNet_raw_data/Task501_Uterus/ImagesTs"
-
-export PATH="/ceph2/grid/home/mpesek/.local/bin:$PATH"
-
-CUDA_VISIBLE_DEVICES=$1
-
-nnUNet_predict -i FOLDER_WITH_TEST_CASES -o OUTPUT_FOLDER_MODEL1 -tr nnUNetTrainerV2 -ctr nnUNetTrainerV2CascadeFullRes -m 3d_fullres -p nnUNetPlansv2.1 -t Task501_Uterus
+nnUNet_predict -i "/home/bonese/Magistrska-nnUNet/data/nnUNet_raw_data_base/nnUNet_raw_data/Task503_Uterus/imagesTs" -o "/home/bonese/Magistrska-nnUNet/data/results_3d_fullres" -tr nnUNetTrainerV2 -ctr nnUNetTrainerV2CascadeFullRes -m 3d_fullres -p nnUNetPlansv2.1 -t Task503_Uterus
